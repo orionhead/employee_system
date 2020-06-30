00e269d78598ae5f164fe5c0479dde5641c6f68e
orionhead <63302426+orionhead@users.noreply.github.com>
2020-06-30 09:29:41 -0500

    new version1.4

--- db_config.py
+++ db_config.py
@@ -3,3 +3,11 @@ DB_SERVER = "localhost"
 DB_USER = "root"
 DB_PASS = ""
 DB = "employee"
+
+
+# For Tab Two
+#1. Add a photo from anywhere on our drive. The photo will be added to the photos directory and extract the file name
+#2. Make sure no text boxes are blank
+#3. An image is added , No text boxes are empty
+#4. Add the record to database
+#5. roload the records when all the records tab is clicked and the displayed record should be the recently added one
--- db_main.py
+++ db_main.py
@@ -61,8 +61,81 @@ def image_path(file_path):
     return ImageTk.PhotoImage(Image.open(file_path))
 
 
+def load_photo_tab_one(file_path):
+    image = image_path(file_path)
+    imgLabelTabOne.configure(image=image)
+    imgLabelTabOne.image = image
+
+
+def scroll_data():
+    fName.set(rows[row_counter][1])
+    fam.set(rows[row_counter][2])
+    job.set(rows[row_counter][3])
+
+    try:
+        ph_path = db_config.PHOTO_DIRECTORY + rows[row_counter][4]
+        load_photo_tab_one(ph_path)
+    except FileNotFoundError:
+        load_photo_tab_one(db_config.PHOTO_DIRECTORY + file_name)
+
+
+def scroll_forward():
+    global row_counter
+    global num_of_rows
+
+    if row_counter >= (num_of_rows - 1):
+        messagebox.showinfo("Database Error", "End of Database")
+    else:
+        row_counter = row_counter + 1
+        scroll_data()
+
+
+def scroll_back():
+    global row_counter
+    if row_counter is 0:
+        messagebox.showinfo("Database Error", "Start of Database")
+    else:
+        row_counter = row_counter - 1
+        scroll_data()
+
+
+def select_image():
+    global image_selected
+    global image_file_name
+    global file_new_home
+    global file_to_copy
+
+    path_to_image = filedialog.askopenfilename(initialdir="/",
+                                               title="Open File",
+                                               filetypes=(("PNGs", "*.png"), ("GIFs", "*.gifs"),
+                                                          ("All Files", "*.*")))
+    try:
+        if path_to_image:
+            image_file_name = os.path.basename(path_to_image)
+            file_new_home = db_config.PHOTO_DIRECTORY + image_file_name
+            file_to_copy = path_to_image
+            image_selected = True
+            load_photo_tab_two(file_to_copy)
+    except IOError as err:
+        image_selected = False
+        messagebox.showinfo("File Error", err)
+
+
+def load_photo_tab_two(file_path):
+    image = image_path(file_path)
+    imageLabelTabTwo.configure(image=image)
+    imageLabelTabTwo.image = image
+
+
 file_name = "default.png"
 path = db_config.PHOTO_DIRECTORY + file_name
+rows = None
+num_of_rows = None
+row_counter = 0
+image_selected = False
+image_file_name = None
+file_new_home = None
+file_to_copy = None
 
 form = tk.Tk()
 form.title('Database Form')
@@ -88,7 +161,8 @@ jobTabTwo = tk.StringVar()
 
 # ADDING WIDGET TO TAB ONE
 firstLabelTabOne = tk.Label(tab1,
-                            text='First name:')  # whats in the bracket is tab1 beacuse it has to be shown on the tab not the form
+                            text='First name:')  # whats in the bracket is tab1 because it has to be shown on the tab
+# not the form
 familyLabelTabOne = tk.Label(tab1, text='Family Name:')
 jobLabelTabOne = tk.Label(tab1, text='Job Title')
 
@@ -101,8 +175,8 @@ jobEntryTabOne = tk.Entry(tab1, textvariable=job)
 imgTabOne = image_path(path)
 imgLabelTabOne = tk.Label(tab1, image=imgTabOne)
 
-buttonForward = tk.Button(tab1, text='Forward')
-buttonBack = tk.Button(tab1, text='Back')
+buttonForward = tk.Button(tab1, text='Forward', command=scroll_forward)
+buttonBack = tk.Button(tab1, text='Back', command=scroll_back)
 
 ####ADD WIDGET TO GRID
 firstLabelTabOne.grid(row=0, column=0, padx=15, pady=15)
@@ -119,7 +193,7 @@ imgLabelTabOne.grid(row=0, column=2, rowspan=3, pady=15)
 buttonBack.grid(row=3, column=0, padx=15, pady=15)
 buttonForward.grid(row=3, column=2, padx=15, pady=15)
 
-# ADDING WIDGETS FOR TAB2
+# ADDING WIDGETS FOR TAB
 firstLabelTabTwo = tk.Label(tab2, text='FirstName')
 familyLabelTabTwo = tk.Label(tab2, text='Family Name')
 jobLabelTabTwo = tk.Label(tab2, text='Job Title')
@@ -134,7 +208,7 @@ imageTabTwo = image_path(path)
 imageLabelTabTwo = tk.Label(tab2, image=imageTabTwo)
 
 buttonCommit = tk.Button(tab2, text='Add Record To Database')
-buttonAddImage = tk.Button(tab2, text='Add Image')
+buttonAddImage = tk.Button(tab2, text='Add Image', command=select_image)
 
 # ADD WIDGET ON TAB TWO
 firstLabelTabTwo.grid(row=0, column=0, padx=15, pady=15)
@@ -156,6 +230,9 @@ if success:
     fName.set(rows[0][1])
     fam.set(rows[0][2])
     job.set(rows[0][3])
+    photo_path = db_config.PHOTO_DIRECTORY + rows[0][4]
+    load_photo_tab_one(photo_path)
+
 tab_parent.pack(expand=1, fill='both')
 
 form.mainloop()
